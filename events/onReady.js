41d4f483ce4be12736ea90276f1ea88780c5c7fa
Evidentsinger14 <Cdelancey186423@gmail.com>
2022-09-07 14:29:39 -0400

    First commit

---
+++ events/onReady.js
@@ -0,0 +1,6 @@
+const { client } = require("../index.js");
+
+client.on("ready", async () => {
+    console.clear();
+    console.log("Bot Ready...")
+})
---
+++ index.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+require("dotenv").config();
+const fs = require("fs");
+
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+
+module.exports.client = client;
+
+client.events = new Discord.Collection();
+
+// Event Handler
+fs.readdirSync("./events/").forEach(file => {
+    const jsFiles = fs.readdirSync("./events/").filter(f => f.split(".").pop() === "js");
+    if ( jsFiles.length <= 0 ) return console.log("[EVENT HANDLER] - Can't find any events.");
+    let check = false;
+    jsFiles.forEach(event => {
+        const eventGet = require(`./events/${event}`);
+        try{
+            client.events.set(eventGet.name, eventGet);
+        } catch (error) {
+            return console.error(error);
+        }
+    })
+});
+
+
+
+
+client.login(process.env.TOKEN);
