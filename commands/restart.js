76f8207b5adeb33f73ded513e11d886ee4fe1768
Evidentsinger14 <Cdelancey186423@gmail.com>
2022-09-19 06:01:14 -0400

    removing emoji reaction

--- commands/restart.js
+++ commands/restart.js
@@ -5,7 +5,6 @@ module.exports.run = async (client, message, args) =>{
     if(message.author.id !== '412070526081695744' || message.author.id !== '417763128604884994'){
         console.log(message.author.id + " Tried to restart. Don't worry, i didn't allow them.")
         message.reply("Sorry boss... no can do. Please contact Divine or Evident to do this.");
-        await message.react('x');
     }
 
 
