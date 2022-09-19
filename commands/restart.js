f2311253948f21aa00c71471cef57e3e2d7fb617
Evidentsinger14 <Cdelancey186423@gmail.com>
2022-09-19 06:07:00 -0400

    update restart.js again?

--- commands/restart.js
+++ commands/restart.js
@@ -2,7 +2,7 @@ const { Message, MessageEmbed } = require("discord.js");
 
 module.exports.run = async (client, message, args) =>{
 
-    if(message.author.id !== '412070526081695744'){
+    if(!(message.author.id === '412070526081695744')){
         console.log(message.author.id + " Tried to restart. Don't worry, i didn't allow them.");
         message.reply("Sorry boss... no can do. Please contact Evident to do this.");
     } else {
