7e0e34710ae8e809fc106e9c8f5b8fa344e4ab7f
Evidentsinger14 <Cdelancey186423@gmail.com>
2022-09-19 06:30:03 -0400

    fixed that, getting ready to implement api.

--- commands/restart.js
+++ commands/restart.js
@@ -2,13 +2,10 @@ const { Message, MessageEmbed } = require("discord.js");
 const BOT_ADMINS = process.env.BOT_ADMINS.split(',');
 
 module.exports.run = async (client, message, args) =>{
-    console.log(BOT_ADMINS);
-
     const senderIsAuthorized = BOT_ADMINS.includes(message.member.id)
 
 
     if(!senderIsAuthorized){
-        console.log(message.author.id + " Tried to restart. Don't worry, i didn't allow them.");
         message.reply("Sorry boss... no can do. Please contact Evident to do this.");
     } else {
         const embed = new MessageEmbed()
