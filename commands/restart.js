4d48842badadee79ea51a536e6119913380a0bbd
Evidentsinger14 <Cdelancey186423@gmail.com>
2022-09-19 06:12:48 -0400

    updated that, this should work for an array of users.

--- commands/restart.js
+++ commands/restart.js
@@ -1,8 +1,15 @@
 const { Message, MessageEmbed } = require("discord.js");
+const BOT_ADMINS = process.env.DISCORD_STAFF_ROLES.split(',');
 
 module.exports.run = async (client, message, args) =>{
+    const senderIsAuthorized = message.member.roles.cache.some(
+        role => BOT_ADMINS.indexOf(role.name) !== -1
+    );
+    if (senderIsAuthorized) {
+        return;
+    }
 
-    if(!(message.author.id === '412070526081695744')){
+    if(!senderIsAuthorized){
         console.log(message.author.id + " Tried to restart. Don't worry, i didn't allow them.");
         message.reply("Sorry boss... no can do. Please contact Evident to do this.");
     } else {
