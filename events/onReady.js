b5a4b6c237415cca767919814fdefc9583990652
Evidentsinger14 <Cdelancey186423@gmail.com>
2023-01-29 19:44:16 -0500

    Added HTTPS request for [Uptime Monitor](https://uptime.ev1dent.dev/status/discordbots).

--- events/onReady.js
+++ events/onReady.js
@@ -1,5 +1,14 @@
 const { client } = require("../index.js");
+const https = require('https');
+const domain = process.env.PUSH_URL
 
 client.on("ready", async () => {
+
+    setInterval(function() {
+        https.get(`${domain}`, (response) =>{
+            // Don't really need to do anything, just want to make sure that this will actually keep an uptime.
+        });
+    }, 6 * 10000);
     console.log("Bot Ready...")
+
 })
